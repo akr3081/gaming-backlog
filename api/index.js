5070a9d840fcebcc7f12a9cf56b4859249724214
Adam Rock <Adamr34@gmail.com>
2025-01-31 20:11:18 -0500

    Reconfiguring for Vercel

--- api/index.js
+++ api/index.js
@@ -2,16 +2,12 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send('Hello World');
+    res.json({status: 200, data: 'Hello World'});
 });
 
 app.get('/heartbeat', (req, res) =>{
     res.json({status: 200, data: `Gaming Backlog service - ${new Date().toLocaleString()}`});
 });
 
-if (!module.parent) {
-  app.listen(3000);
-  console.log('Gaming Backlog service started on port 3000...');
-};
-
+app.listen(3000, () => console.log('Gaming Backlog service started on port 3000...'));
 module.exports = app;
