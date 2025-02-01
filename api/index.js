d1bb19e45f64e4bc7e23959c9df380ac8c5bd3c0
Adam Rock <Adamr34@gmail.com>
2025-01-31 22:06:36 -0500

    Added prettier/htlb packages and set up search endpoint

--- api/index.js
+++ api/index.js
@@ -1,13 +1,59 @@
 const express = require('express');
+
+const hltb = require('../utils/HowLongToBeat');
 const app = express();
 
-app.get('/', (req, res) =>{
-    res.json({status: 200, data: 'Hello World'});
+app.get('/heartbeat', (req, res) => {
+  res.json({
+    status: 200,
+    data: `Gaming Backlog service - ${new Date().toLocaleString()}`,
+  });
 });
 
-app.get('/heartbeat', (req, res) =>{
-    res.json({status: 200, data: `Gaming Backlog service - ${new Date().toLocaleString()}`});
+app.get('/search', (req, res) => {
+  const searchTerm = req?.query?.searchTerm;
+  if (!searchTerm) {
+    return res.json({
+      status: 400,
+      data: `Query param 'searchTerm' is required`,
+    });
+  }
+
+  hltb
+    .search(searchTerm)
+    .then((result) => {
+      const gameId = result?.data?.[0]?.game_id;
+
+      if (!gameId) {
+        return res.json({
+          status: 404,
+          data: `Game not found for searchTerm '${searchTerm}'`,
+        });
+      } else {
+        return gameId;
+      }
+    })
+    .then((result) => {
+      hltb.details(result).then((result) => {
+        return res.json({
+          status: 200,
+          data: result,
+        });
+      });
+    })
+    .catch((error) => {
+      return res.json({
+        status: 500,
+        error: `Gaming Backlog Search Error - ${error}`,
+      });
+    });
 });
 
-app.listen(3000, () => console.log('Gaming Backlog service started on port 3000...'));
+app.get('*', (req, res) => {
+  res.json({ status: 404, data: 'Invalid Route' });
+});
+
+app.listen(3000, () =>
+  console.log('Gaming Backlog service started on port 3000...')
+);
 module.exports = app;
---
+++ utils/HowLongToBeat.js
@@ -0,0 +1,72 @@
+const axios = require('axios');
+const UserAgent = require('user-agents');
+
+const hltb = require('howlongtobeat');
+const hltbService = new hltb.HowLongToBeatService();
+
+const SEARCH_URL = `https://howlongtobeat.com/api/s`;
+
+const defaultPayload = {
+  searchType: 'games',
+  searchTerms: [],
+  searchPage: 1,
+  size: 20,
+  searchOptions: {
+    games: {
+      userId: 0,
+      platform: '',
+      sortCategory: 'popular',
+      rangeCategory: 'main',
+      rangeTime: { min: null, max: null },
+      gameplay: { perspective: '', flow: '', genre: '', difficulty: '' },
+      rangeYear: { min: '', max: '' },
+      modifier: '',
+    },
+    users: { sortCategory: 'postcount' },
+    lists: { sortCategory: 'follows' },
+    filter: '',
+    sort: 0,
+    randomizer: 0,
+  },
+  useCache: true,
+};
+
+// Search function had to be rebuilt from scratch since HLTB library is outdated
+const search = async (searchTerm) => {
+  const search = { ...defaultPayload, searchTerms: [searchTerm] };
+  const url = `${SEARCH_URL}/5b26492381a39f41`;
+
+  const headers = {
+    'User-Agent': new UserAgent().toString(),
+    'content-type': 'application/json',
+    origin: 'https://howlongtobeat.com/',
+    referer: 'https://howlongtobeat.com/',
+  };
+
+  console.log(`HLTB Search request at ${url}...`);
+
+  try {
+    const result = await axios.post(url, search, {
+      headers,
+      timeout: 30000,
+    });
+    return result.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const details = async (gameId) => {
+  try {
+    console.log(`Fetching game results for ${gameId}...`);
+    const result = await hltbService.detail(gameId);
+    return result;
+  } catch (error) {
+    throw new Error(`HLTB details error - ${error}`);
+  }
+};
+
+module.exports = {
+  search,
+  details,
+};
